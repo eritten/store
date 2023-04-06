55fe639f7a8ad895721cfaa11971d0c4144e71fa
= <=>
2023-04-06 05:26:32 -0200

    adding the templates files

---
+++ home/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, get_object_or_404
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, "about.html")
+
+def contact(request):
+    return render(request, "contact.html")
+
+def detail(request, title, year,  year, month, day):
+    project = get_object_or_404(Project, title=title, date__year=year, date__month=month, date__day=day)
+    return render(request, "detail.html", {"project":project})
+def terms(request):
+    return render(request, "terms.html")
+
+def privacy(request):
+    return render(request, "privacy.html")
--- store/urls.py
+++ store/urls.py
@@ -1,21 +1,16 @@
-"""store URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from home import views
 
 urlpatterns = [
+path("", views.home, name="home"),
+path("about/", views.about, name="about"),
+path("contact/", views.contact, name="contact"),
+path("detail/<str:title>/<int:year>/<int:month>/<int:day>/", views.detail, name="detail"),
+path("privacy/", views.privacy, name="privacy"),
+path("terms/", views.terms, name="terms"),
+
     path('admin/', admin.site.urls),
 ]
