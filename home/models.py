55d31f6d3aa3649221ce6e3bdfdbbcd98ce18d98
= <=>
2023-04-17 00:30:21 -0200

    add changes

--- home/models.py
+++ home/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils.timezone import now
+from django.urls import reverse
+
 
 # Create your models here.
+
+class Project(models.Model):
+    project_name = models.CharField(max_length=200)
+    project_description = models.TextField()
+    project_image = models.ImageField(upload_to="images")
+    date_release = models.DateTimeField(default=now)
+    def __str__(self):
+        return self.project_name
+#    def get_absolute_url(self):
+#        return reverse()
--- home/views.py
+++ home/views.py
@@ -10,9 +10,10 @@ def about(request):
 def contact(request):
     return render(request, "contact.html")
 
-def detail(request, title, year,  year, month, day):
-    project = get_object_or_404(Project, title=title, date__year=year, date__month=month, date__day=day)
-    return render(request, "detail.html", {"project":project})
+#def detail(request, title, year,  year, month, day):
+#    project = get_object_or_404(Project, title=title, date__year=year, date__month=month, date__day=day)
+#    return render(request, "detail.html", {"project":project})
+
 def terms(request):
     return render(request, "terms.html")
 
